5044f83075f2b67626d95388b9f51a1d7e2d642e
Adam Donaghy <donaghyam@gmail.com>
2022-01-13 13:43:51 -0600

    functioning

---
+++ main.js
@@ -0,0 +1,17 @@
+const add = (firstValue, secondValue) => {
+    const output = firstValue + secondValue
+    return output
+}
+
+const additionOutput = add(9999,238)
+
+console.log(additionOutput)
+
+const multiply = (number) => {
+    const output = number * 2
+    return output
+}
+
+const multiplicationOutput = multiply(87)
+
+console.log(multiplicationOutput)
